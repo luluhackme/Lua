8c687cbde0cab5e43c0223678e356f042a033f4e
Lucas SOLERA <179798293+luluhackme@users.noreply.github.com>
2026-02-12 12:47:03 +0100

    Refactor acronym function to handle punctuation and spaces

--- acronym.lua
+++ acronym.lua
@@ -1,7 +1,13 @@
 function acronym(s)
-  string = ''
-  for c in string.gmatch(s, '%u') do
-    string = string .. c
+  local nString = ''
+  j = 1
+  for c in string.gmatch(s, '.') do
+    if (string.match((string.sub(s, j-1, j-1)), '%p') or string.match((string.sub(s, j-1, j-1)), '%s')) and string.match(c, '%a') then
+      nString = nString .. c
+    elseif j == 1 then
+      nString = nString .. c
+    end
+    j = j+1
   end
-  return string
+  return string.upper(nString)
 end
